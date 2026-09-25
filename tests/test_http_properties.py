363a8a5b8ba578e103936861d838f2937ccf830c
aipusulaofficial-cyber <aipusulaofficial@gmail.com>
2026-09-25 18:33:54 +0300

    style: fix remaining lint findings

--- tests/test_http_properties.py
+++ tests/test_http_properties.py
@@ -1,6 +1,9 @@
-from hypothesis import given, strategies as st
 from fastapi.testclient import TestClient
+from hypothesis import given
+from hypothesis import strategies as st
+
 from service import app
+
 
 c = TestClient(app)
 
